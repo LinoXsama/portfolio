4e28685c75dfe0698d52b55b670ad1c179baf72f
Merlin Migan <merlin.migan@gmail.com>
2024-07-16 11:49:27 +0200

    Initialisation des composants Header, Footer, ThemeProvider et ThemeToggler !

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +13,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider>
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ components/ThemeToggler.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import {} from './ui/button';
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+
+
+const ThemeToggler = () => {
+  return (
+    <div>ThemeToggler</div>
+  );
+};
+
+export default ThemeToggler;
