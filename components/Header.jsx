e23d24fed3d217c25d4b2167019f9c6de37b2900
Merlin Migan <merlin.migan@gmail.com>
2024-07-16 16:12:26 +0200

    Ecrans projects et contact créés !

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,30 @@
+'use client';
+
+import React, { useState, useEffect } from "react";
+
+import Logo from "./Logo";
 import ThemeToggler from "./ThemeToggler";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 
 const Header = () => {
   return (
     <header>
-      <ThemeToggler />
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex items-center gap-x-6">
+          {/* Navbar */}
+            <Nav containerStyles="hidden xl:flex gap-x-8 items-center" />
+            <ThemeToggler />
+            {/* Mobile Navbar */}
+              <div className="xl:hidden">
+                <MobileNav />
+              </div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
