c74706df13075821b2afac6f130d6850b06cc6ac
Merlin Migan <merlin.migan@gmail.com>
2024-07-17 12:48:25 +0200

    Développement du composant Socials en cours ...1 !

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -8,9 +8,40 @@ import {
   RiInstallFill
 } from "react-icons/ri";
 
+import Link from "next/link";
 
-const Socials = () => {
-  return <div>Socials</div>;
+const icons = [
+  {
+    path: '/',
+    path: <RiYoutubeFill />,
+  },
+  {
+    path: '/',
+    path: <RiLinkedinFill />,
+  },
+  {
+    path: '/',
+    path: <RiGithubFill />,
+  },
+  {
+    path: '/',
+    path: <RiFacebookFill />,
+  },
+  {
+    path: '/',
+    path: <RiInstallFill />,
+  },
+];
+
+
+const Socials = ({ containerStyles, iconsStyles }) => {
+  return (
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => {
+        
+      })}
+    </div>
+  );
 };
 
 export default Socials;
