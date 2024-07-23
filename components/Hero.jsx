5525a32de273b5874b1a2cd5914b5f733a169889
Merlin Migan <merlin.migan@gmail.com>
2024-07-23 15:22:39 +0200

    Composants Badge et MobileNav terminés !

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -50,12 +50,33 @@ const Hero = () => {
           {/* image */}
           <div className="hidden xl:flex relative">
             {/* badge 1 */}
-            <Badge />
+            <Badge
+              containerStyles="absolute top-[24%] -left-[5rem]"
+              icon={<RiBriefcase4Fill />}
+              endCountNum={3}
+              badgeText="Years of experience"
+            />
+            {/* badge 2 */}
+            <Badge
+              containerStyles="absolute top-[80%] -left-[5rem]"
+              icon={<RiTodoFill />}
+              endCountNum={6}
+              endCountText='k'
+              badgeText="Finished projects"
+            />
+            {/* badge 3 */}
+            <Badge
+              containerStyles="absolute top-[55%] -right-8"
+              icon={<RiTeamFill />}
+              endCountNum={9}
+              endCountText='k'
+              badgeText="Happy clients"
+            />
             <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
-              <DevImg
-                containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
-                imgSrc="/hero/developer.png"
-              />
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/developer.png"
+            />
           </div>
         </div>
         {/* icon */}
