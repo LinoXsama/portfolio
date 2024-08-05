c7502a0578a2301825a0eb6cd6e917eb2e06191d
Merlin Migan <merlin.migan@gmail.com>
2024-08-05 15:49:04 +0200

    Pause 2 - About.jsx !

--- components/About.jsx
+++ components/About.jsx
@@ -104,13 +104,48 @@ const About = () => {
       return arr.find((item) => item.title === title);
    };
 
-   console.log(getData(qualificationData, 'experience'))
+   // console.log(getData(qualificationData, 'experience'))
    return (
       <section className="xl:h-[860px] pb-12 xl:py-24">
          <div className="container mx-auto">
             <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
                About me
             </h2>
+            {/* Image */}
+            <div className="flex flex-col xl:flex-row">
+               <div className="hidden xl:flex flex-1 relative">
+                  <DevImg containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative' imgSrc='/about/developer.png' />
+               </div>
+               {/* tabs */}
+               <div className="flex-1">
+                  <Tabs defaultValue="personal">
+                     <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none">
+                        <TabsTrigger className="w-[162px] xl:w-auto" value="personal">Informations</TabsTrigger>
+                        <TabsTrigger className="w-[162px] xl:w-auto" value="qualifications">Qualifications</TabsTrigger>
+                        <TabsTrigger className="w-[162px] xl:w-auto" value="skills">Compétences</TabsTrigger>
+                     </TabsList>
+                     {/* tabls content */}
+                     <div className="text-lg mt-12 xl:mt-8">
+                        {/* personal */}
+                        <TabsContent value="personal">
+                           <div>
+                              <h3>Services irréprochables</h3>
+                              <p>I specialize in crafting intuitive websites with cutting-edge technology, delivering dynamic and engaging
+                              user experiences.
+                              </p>
+                           </div>
+                        </TabsContent>
+                        {/* qualifications */}
+                        <TabsContent value="qualifications">
+                           Qualifications
+                        </TabsContent>
+                        <TabsContent value="skills">
+                           Compétences
+                        </TabsContent>
+                     </div>
+                  </Tabs>
+               </div>
+            </div>
          </div>
       </section>
    );
