a57461f493251879bfacb2702bbf33027cfa3bb4
Merlin Migan <merlin.migan@gmail.com>
2024-08-05 12:42:19 +0200

    Ajout du tableau d'objets skillData regroupant mes compétences !

--- components/About.jsx
+++ components/About.jsx
@@ -6,36 +6,69 @@ import { User2, MailIcon, HomeIcon, PhoneCall, GraduationCap, Calendar, Briefcas
 
 
 const infoData = [
-    {
-        icon: <User2 size={20} />,
-        text: 'Merlin Migan',
-    },
-    {
-        icon: <PhoneCall size={20} />,
-        text: '06 59 34 82 49',
-    },
-    {
-        icon: <MailIcon size={20} />,
-        text: 'merlin.migan@gmail.com',
-    },
-    {
-        icon: <Calendar size={20} />,
-        text: 'Né le 18 Mai 1994',
-    },
-    {
-        icon: <GraduationCap size={20} />,
-        text: 'Certificat professionnel de développeur web & mobile',
-    },
-    {
-        icon: <HomeIcon size={20} />,
-        text: '1 Bis Quai des Tanneurs, 34090 Montpellier',
-    },
+   {
+      icon: <User2 size={20} />,
+      text: 'Merlin Migan',
+   },
+   {
+      icon: <PhoneCall size={20} />,
+      text: '06 59 34 82 49',
+   },
+   {
+      icon: <MailIcon size={20} />,
+      text: 'merlin.migan@gmail.com',
+   },
+   {
+      icon: <Calendar size={20} />,
+      text: 'Né le 18 Mai 1994',
+   },
+   {
+      icon: <GraduationCap size={20} />,
+      text: 'Certificat professionnel de développeur web & mobile',
+   },
+   {
+      icon: <HomeIcon size={20} />,
+      text: '1 Bis Quai des Tanneurs, 34090 Montpellier, France',
+   },
+];
+
+const qualificationData = [
+   {
+      title: 'education',
+      data: [
+         {
+            university: 'Studi',
+            qualifications: 'Certificat professionnel de développeur web & mobile',
+            years: '2023 - 2024',
+         },
+      ],
+   },
+];
+
+const skillData = [
+   {
+      title: 'skills',
+      data: [
+         {
+            name: 'HTML, CSS',
+         },
+         {
+            name: 'JavaScript',
+         },
+         {
+            name: 'React & Next JS',
+         },
+         {
+            name: 'React Native avec Expo',
+         },
+      ],
+   },
 ];
 
 const About = () => {
-    return (
-        <div>About</div>
-    );
+   return (
+      <div>About</div>
+   );
 };
 
 export default About;
