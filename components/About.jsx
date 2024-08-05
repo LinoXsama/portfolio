e0632ccfb8bd7abc049da63fa27d1f51b83c67ff
Merlin Migan <merlin.migan@gmail.com>
2024-08-05 13:34:23 +0200

    Pause - About.jsx !

--- components/About.jsx
+++ components/About.jsx
@@ -106,9 +106,9 @@ const About = () => {
 
    console.log(getData(qualificationData, 'experience'))
    return (
-      <section>
+      <section className="xl:h-[860px] pb-12 xl:py-24">
          <div className="container mx-auto">
-            <h2 className="section-title">
+            <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
                About me
             </h2>
          </div>
