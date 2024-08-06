51981160ecf9af9348be0945f02d39a083ee0412
Merlin Migan <merlin.migan@gmail.com>
2024-08-06 20:58:14 +0200

    Composant About.jsx terminé !

--- components/About.jsx
+++ components/About.jsx
@@ -34,16 +34,16 @@ const infoData = [
 
 const qualificationData = [
    {
-      title: 'education',
+      title: 'formation',
       data: [
          {
             university: 'Studi',
-            qualifications: 'Certificat professionnel de développeur web & mobile',
+            qualification: 'Certificat professionnel de développeur web & mobile',
             years: '2023 - 2024',
          },
          {
             university: 'Google Career Certificates',
-            qualifications: 'AI Essentials',
+            qualification: 'AI Essentials',
             years: '2024',
          },
       ],
@@ -159,9 +159,9 @@ const About = () => {
                                  My Awesome Journey
                               </h3>
                               {/* experience & education wrapper */}
-                              <div className="">
+                              <div className="grid md:grid-cols-2 gap-y-8">
                                  {/* experience */}
-                                 <div className="">
+                                 <div className="flex flex-col gap-y-6">
                                     <div className="flex gap-x-4 items-center text-[22px] text-primary">
                                        <Briefcase />
                                        <h4 className="capitalize font-medium">
@@ -174,8 +174,10 @@ const About = () => {
                                           const { company, role, years } = item;
 
                                           return (
-                                             <div className="flex gap-x-8 group bg-yellow-100" key={index}>
-                                                <div className="h-[84px] w-[1px] bg-border relative ml-2"></div>
+                                             <div className="flex gap-x-8 group " key={index}>
+                                                <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                   <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                                </div>
                                                 <div className="">
                                                    <div className="font-semibold text-xl leading-none mb-2">
                                                       {company}
@@ -193,12 +195,85 @@ const About = () => {
                                     </div>
                                  </div>
                                  {/* education */}
-                                 <div className="">education</div>
+                                 <div className="flex flex-col gap-y-6">
+                                    <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                                       <GraduationCap size={28} />
+                                       <h4 className="capitalize font-medium">
+                                          {getData(qualificationData, 'formation').title}
+                                       </h4>
+                                    </div>
+                                    {/* list */}
+                                    <div className="flex flex-col gap-y-8">
+                                       {getData(qualificationData, 'formation').data.map((item, index) => {
+                                          const { university, qualification, years } = item;
+
+                                          return (
+                                             <div className="flex gap-x-8 group " key={index}>
+                                                <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                                   <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                                </div>
+                                                <div className="">
+                                                   <div className="font-semibold text-xl leading-none mb-2">
+                                                      {university}
+                                                   </div>
+                                                   <div className="text-lg leading-none text-muted-foreground mb-4">
+                                                      {qualification}
+                                                   </div>
+                                                   <div className="text-base font-medium">
+                                                      {years}
+                                                   </div>
+                                                </div>
+                                             </div>
+                                          );
+                                       })}
+                                    </div>
+                                 </div>
                               </div>
                            </div>
                         </TabsContent>
+                        {/* skills */}
                         <TabsContent value="skills">
-                           Compétences
+                           <div className="text-center xl:text-left">
+                              <h3 className="h3 mb-8">Mes outils au quotidien ?</h3>
+                              {/* skills */}
+                              <div className="mb-16">
+                                 <h4 className="text-xl font-semibold mb-2">Compétences</h4>
+                                 <div className="border-b border-border mb-4"></div>
+                                 {/* skill list */}
+                                 <div className="">
+                                    {getData(skillData, 'skills').data.map((item, index) => {
+                                       const { name } = item;
+
+                                       return (
+                                          <div className="w-2/4 text-center xl:text-left mx-auto xl:mx-0" key={index}>
+                                             <div className="font-medium">
+                                                {name}
+                                             </div>
+                                          </div>
+                                       );
+                                    })}
+                                 </div>
+                              </div>
+                              {/* tools */}
+                              <div>
+                                 <h4 className="text-xl font-semibold mb-2 xl:text-left">
+                                    Outils
+                                 </h4>
+                                 <div className="border-b border-border mb-4"></div>
+                                 {/* tool list */}
+                                 <div className="flex gap-x-8 justify-center xl:justify-start">
+                                    {getData(skillData, 'tools').data.map((item, index) => {
+                                       const { imgPath } = item;
+
+                                       return (
+                                          <div className="" key={index}>
+                                             <Image src={imgPath} width={48} height={48} alt='' priority />
+                                          </div>
+                                       );
+                                    })}
+                                 </div>
+                              </div>
+                           </div>
                         </TabsContent>
                      </div>
                   </Tabs>
