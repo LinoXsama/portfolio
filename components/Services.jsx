54ff1b9d9387f879cf96bee6df6534888c0c5ba7
Merlin Migan <merlin.migan@gmail.com>
2024-08-09 13:27:18 +0200

    Composant Services terminé  !

--- components/Services.jsx
+++ components/Services.jsx
@@ -5,7 +5,7 @@ import {
    CardDescription,
    CardHeader,
    CardTitle
-} from 'lucide-react';
+} from '@/components/ui/card';
 
 const servicesData = [
    {
@@ -28,13 +28,33 @@ const servicesData = [
 const Services = () => {
    return (
       <section className='mb-12 xl:mb-36'>
-         <div className="container mx-auto">
+         <div className='container mx-auto'>
             <h2 className='section-title mb-12 xl:mb-24 text-center mx-auto'>
                Mes Services
             </h2>
             {/* grid items */}
-            <div className="">
-               grid items
+            <div className='grid xl:grid-cols-3 justify-center gap-y-24 xl:gap-x-8'>
+               {
+                  servicesData.map((item, index) => {
+                     return (
+                        <Card key={index} className='w-full max-w-[424px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative'>
+                           <CardHeader className='text-primary absolute -top-[60px]'>
+                              <div className='w-[140px] h-[80px] bg-white dark:bg-background flex justify-center items-center'>
+                                 {item.icon}
+                              </div>
+                           </CardHeader>
+                           <CardContent className='text-center'>
+                              <CardTitle className='mb-4'>
+                                 {item.title}
+                              </CardTitle>
+                              <CardDescription className='text-lg'>
+                                 {item.description}
+                              </CardDescription>
+                           </CardContent>
+                        </Card>
+                     );
+                  })
+               }
             </div>
          </div>
       </section>
