dbb7f73b0ac20a371331436a8cc8eb3b09a0f042
Merlin Migan <slavikzaya@gmail.com>
2024-08-11 23:35:28 +0200

    Tentative d'intégration des liens des projets et de leur github. Mais l'élément Link ne s'affiche pas !

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -21,9 +21,20 @@ const ProjectCard = ({ project }) => {
             priority
           />
           {/* slider's buttons */}
-          <Link href="">
-
-          </Link>
+          <div className=''>
+            <Link
+              href={project.link}
+              className='bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200'
+            >
+              <Link2Icon className='text-white' />
+            </Link>
+            <Link
+              href={project.github}
+              className='bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200'
+            >
+              <Github className='text-white' />
+            </Link>
+          </div>
         </div>
       </CardHeader>
 
