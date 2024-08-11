8723ebb76c15615f000f23396ec21caef1648fdf
Merlin Migan <slavikzaya@gmail.com>
2024-08-11 12:56:27 +0200

    Intégration d'un slider dans le composant ProjectCard !

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const ProjectCard = () => {
+import { Card, CardHeader } from './ui/card';
+import { Github, Link2Icon } from 'lucide-react';
+import { Badge } from './ui/badge';
+
+
+const ProjectCard = ({ project }) => {
+  console.log(project)
   return (
-    <div>ProjectCard</div>
+    <Card>
+      <CardHeader>Card Header</CardHeader>
+
+        <div className='h-full px-8 py-6'>
+          <Badge>{project.category}</Badge>
+          <h4 className='h4 mb-1'>{project.name}</h4>
+          <p className='text-muted-foreground text-lg'>
+            {project.description}
+          </p>
+        </div>
+    </Card>
   );
 };
 
--- components/Work.jsx
+++ components/Work.jsx
@@ -2,6 +2,8 @@
 import Link from 'next/Link';
 import { Button } from './ui/Button';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 // import swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -61,7 +63,7 @@ const Work = () => {
 		<section className='relative mb-12 xl:mb-48'>
 			<div className="container mx-auto">
 				{/* text */}
-				<div className='bg-pink-100 max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start'>
+				<div className='max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start' >
 					<h2 className='section-title mb-4'>Projets récents</h2>
 					<p className='subtitle mb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
@@ -70,7 +72,29 @@ const Work = () => {
 					</Link>
 				</div>
 				{/* slider */}
-				<div>slider</div>
+				<div className='xl:max-w-[1000px] xl:absolute right-0 top-0'>
+					<Swiper
+						className='h-[480px]'
+						slidesPerView={1}
+						breakpoints={{
+							640: {
+								slidesPerView: 2,
+							}
+						}}
+						spaceBetween={30}
+						modules={[Pagination]}
+						pagination={{ clickable: true }}
+					>
+						{/* show only the first 4 projects for the slider */}
+						{projectData.slice(0.4).map((project, index) => {
+							return (
+								<SwiperSlide key={index}>
+									<ProjectCard project={project} />
+								</SwiperSlide>
+							);
+						})}
+					</Swiper>
+				</div>
 			</div>
 		</section>
 	);
