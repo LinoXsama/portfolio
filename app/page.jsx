5a9acac4caed9391bb2241285c9ed3d0fc818e46
Merlin Migan <slavikzaya@gmail.com>
2024-08-12 13:51:59 +0200

    Intégration du composant Cta au layout de l'application !
    J'ai ajouté Cta à page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
 import Reviews from "@/components/Reviews";
+import Cta from "@/components/Cta";
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <Services />
       <Work />
       <Reviews />
+      <Cta />
     </main>
   );
 };
