fe95165336a6f242b27a29879688e323b55f8e8c
Merlin Migan <slavikzaya@gmail.com>
2024-08-12 14:49:22 +0200

    Le composant Cta.jsx est terminé !

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -1,9 +1,23 @@
-
+import { Button } from './ui/button';
+import Link from 'next/link';
 
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <section className='py-24 bg-tertiary dark:bg-secondary/40'>
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center">
+          <h2 className="h2 max-w-xl text-center mb-8">
+            Préparez-vous à transformer vos idées en réalités. 
+            Je suis là pour vous y aider !
+          </h2>
+
+          <Link href='/contact'>
+            <Button>Contactez-moi</Button>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
