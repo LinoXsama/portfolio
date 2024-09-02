708bebef6c611e9f733a139bcc9273d4bd04dad3
Merlin Migan <slavikzaya@gmail.com>
2024-09-02 17:14:36 +0200

    Développement du composant Contact !
    - Je sous composant Form est terminé
    - J'ai du modifié le composant custom textara récupéré sur Shadcn

--- components/Form.jsx
+++ components/Form.jsx
@@ -8,12 +8,26 @@ import { User, MailIcon, ArrowRightIcon, MessageSquare } from 'lucide-react';
 
 const Form = () => {
   return (
-    <form>
+    <form className='flex flex-col gap-y-4'>
       {/* input */}
       <div className='relative flex items-center'>
-         <Input type='name' id='name' placeholder='Name' />
+         <Input type='name' id='name' placeholder='Nom' />
          <User className='absolute right-6' size={20} />
       </div>
+      {/* input */}
+      <div className='relative flex items-center'>
+         <Input type='name' id='email' placeholder='Email' />
+         <MailIcon className='absolute right-6' size={20} />
+      </div>
+      {/* textarea */}
+      <div className='relative flex items-center'>
+         <Textarea placeholder='Ecrivez votre message ici...' />
+         <MessageSquare className='absolute top-4 right-6' size={20} />
+      </div>
+
+      <Button className='flex items-center max-w-[166px] gap-x-1'>
+        Let's Talk <ArrowRightIcon size={20} />
+      </Button>
     </form>
   )
 }
